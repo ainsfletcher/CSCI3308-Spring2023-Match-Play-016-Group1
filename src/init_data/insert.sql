08da7f82d52f8d42a338bd6108dfe03cc5421fc4
Adam Carlson <bronadam25@gmail.com>
2023-04-16 17:55:30 -0600

    Adding insert.sql

--- src/init_data/insert.sql
+++ src/init_data/insert.sql
@@ -1,1 +1,3 @@
-INSERT INTO users ()
+INSERT INTO users (name, username, password, handicap, avg_drinks, age, home_course, movement, bio) VALUES 
+(Ron, ron123, 123, 10.2, 2, 21, The Country Club, walk, I am a cool guy),
+(John, john123, 123, 10.2, 2, 21, The Country Club, cart, I am the coolest guy),
