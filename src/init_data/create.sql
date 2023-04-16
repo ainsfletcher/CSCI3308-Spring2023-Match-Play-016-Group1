613f6c13cfc2c8a76bb39ee42a5ba83f297312c3
Adam Carlson <bronadam25@gmail.com>
2023-04-16 17:15:12 -0600

    Updating users table

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,6 +1,14 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
   user_id VARCHAR(50) PRIMARY KEY,
-  password CHAR(60) NOT NULL
+  name VARCHAR(50) NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  password CHAR(50) NOT NULL,
+  handicap DOUBLE PRECISION NOT NULL,
+  avg_drinks SMALLINT NOT NULL,
+  age SMALLINT NOT NULL,
+  home_course VARCHAR(50) NOT NULL,
+  movement(walk/cart) VARCHAR(10) NOT NULL,
+  bio VARCHAR(500) NOT NULL,
 
-)
+);
